4bebd6cdfe96493c050f6042a7801e155f832f83
Mohit Hosakatte Niranjana Murthy <mohithn7@gmail.com>
2024-09-12 23:14:36 +0000

    left_Arr resolved

--- hw2_debugging.py
+++ hw2_debugging.py
@@ -11,17 +11,17 @@ def merge_sort(arr):
 
     return recombine(merge_sort(arr[:half]), merge_sort(arr[half:]))
 
-def recombine(leftArr, rightArr):
+def recombine(left_arr, rightArr):
     """
     Merge Sort helper
     """
     leftIndex = 0
     rightIndex = 0
-    mergeArr = [None] * (len(leftArr) + len(rightArr))
-    while leftIndex < len(leftArr) and rightIndex < len(rightArr):
-        if leftArr[leftIndex] < rightArr[rightIndex]:
+    mergeArr = [None] * (len(left_arr) + len(rightArr))
+    while leftIndex < len(left_arr) and rightIndex < len(rightArr):
+        if left_arr[leftIndex] < rightArr[rightIndex]:
             rightIndex += 1
-            mergeArr[leftIndex + rightIndex] = leftArr[leftIndex]
+            mergeArr[leftIndex + rightIndex] = left_arr[leftIndex]
         else:
             leftIndex += 1
             mergeArr[leftIndex + rightIndex] = rightArr[rightIndex]
@@ -29,8 +29,8 @@ def recombine(leftArr, rightArr):
     for i in range(rightIndex, len(rightArr)):
         mergeArr[leftIndex + rightIndex] = rightArr[i]
 
-    for i in range(leftIndex, len(leftArr)):
-        mergeArr[leftIndex + rightIndex] = leftArr[i]
+    for i in range(leftIndex, len(left_arr)):
+        mergeArr[leftIndex + rightIndex] = left_arr[i]
 
     return mergeArr
 
