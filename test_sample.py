eaa21514db7bacc86b199a09c0fe25d647fcacff
Nikhilesh Cherukuri <44161680+nikki1234567@users.noreply.github.com>
2024-09-12 23:41:28 +0000

    merge_sort renamed

--- test_sample.py
+++ test_sample.py
@@ -1,14 +1,14 @@
 import pytest
-from hw2_debugging import mergeSort
+from hw2_debugging import merge_sort
 
 def test_one():
     arr = [4,78,31,2,5,9]
-    assert mergeSort(arr) == [2,4,5,9,31,78]
+    assert merge_sort(arr) == [2,4,5,9,31,78]
 
 def test_two():
     arr = [4]
-    assert mergeSort(arr) == [4]
+    assert merge_sort(arr) == [4]
 
 def test_three():
     arr = [21,7,43,3,71]
-    assert mergeSort(arr) == [3,7,21,43,71]
+    assert merge_sort(arr) == [3,7,21,43,71]
